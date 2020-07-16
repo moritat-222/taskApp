04034fb3d9040ab694aa8714c5bcd8f3740418d0
メグミ <morime00@meguminoMacBook-Pro.local>
2020-07-16 20:53:58 +0900

    herokuエラーの修正

--- src/index.js
+++ src/index.js
@@ -31,9 +31,6 @@ const Task = require('./models/task')
 const User = require('./models/user')
 
 const main = async() => {
-    //const task = await Task.findById('5ef80122a1c42a63983ff796')
-    //await task.populate('owner').execPopulate()
-    //console.log(task.owner)
 
     const user = await User.findById('5ef7fca6f23a596376eddb3a')
     await user.populate('tasks').execPopulate()
@@ -41,29 +38,3 @@ const main = async() => {
 }
 
 main()
-
-//const pet = {
-//    name: 'Hal'
-//}
-//
-//pet.toJSON = function(){
-//    return {}
-//}
-//
-//console.log(JSON.stringify(pet))
-
-
-//const jwt = require('jsonwebtoken')
-//const myFunction = async() => {
-//    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse',{expiresIn: '7 days'})//第一引数はデータベースの中の一意な値。第二引数はなんでも良いので好きな文字.これが署名になる
-//    console.log(token)
-//
-//    const data = jwt.verify(token, 'thisismynewcourse')
-//    console.log(data)
-//}
-//
-//myFunction()
-
-//インクリプションアルゴリズムを使うと、ハッシュ化したものを元に戻せる
-//mypsaa => brabra(元には戻せない仕様になっている。)
-//インクリプションは、元の値をとっておいて、ハッシュ化したらそのハッシュと同じになるか確認している
